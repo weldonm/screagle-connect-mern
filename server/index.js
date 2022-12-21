835ad2d7ac5ef7b1bdaccac3cfaf98c50b6bf817
Michael Weldon <102847321+weldonm@users.noreply.github.com>
2022-12-21 13:19:19 -0600

    models/routes/middleware

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { register } from './controllers/auth.js'
+import authRoutes from './routes/auth.js'
 
 // Configs
 const __filename = fileURLToPath(import.meta.url)
@@ -35,6 +37,12 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({ storage })
+
+app.post('/auth/register', upload.single('picture'), register)
+
+// Routes
+app.use('/auth', authRoutes)
+
 
 // Mongoose 
 const PORT = process.env.PORT || 5001
